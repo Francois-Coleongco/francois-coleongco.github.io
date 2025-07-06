fa76823d04a35a5ae9444df37e13223b4e0f0af9
Francois Coleongco <christien.coleongco@gmail.com>
2025-07-05 22:35:59 -0600

    first commit

---
+++ src/pages/projects/data.ts
@@ -0,0 +1,38 @@
+import type { ProjectData } from '@/types'
+
+export const projectData: ProjectData = [
+  {
+    title: 'Work In Progress',
+    projects: [
+      {
+        text: 'Multithreaded Encrypted File Server and Client (C++)',
+        description: 'with the ability to concurrently handle encrypted client connections',
+        icon: 'i-carbon-campsite',
+        href: 'https://github.com/francois-coleongco/MEFSC',
+      },
+    ],
+  },
+  {
+    title: 'Complete',
+    projects: [
+      {
+        text: 'Multithreaded Encrypted File Server and Client (C++)',
+        description: 'with the ability to concurrently handle encrypted client connections',
+        icon: 'i-carbon-campsite',
+        href: 'https://github.com/francois-coleongco/MEFSC',
+      },
+      {
+        text: 'Project Name',
+        description: 'Your project description information is a long piece of text.',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+      {
+        text: 'Project Name',
+        description: 'Your project description information is a long piece of text.',
+        icon: 'i-carbon-campsite',
+        href: '/',
+      },
+    ],
+  },
+]
