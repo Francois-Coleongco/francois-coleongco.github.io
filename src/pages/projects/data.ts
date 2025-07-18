2cf9f1b65f38fa96d1268766dc356cdd067d345b
Francois Coleongco <christien.coleongco@gmail.com>
2025-07-17 19:48:57 -0600

    added project. need to figure out how the icons work i dont like campsite

--- src/pages/projects/data.ts
+++ src/pages/projects/data.ts
@@ -1,38 +1,33 @@
 import type { ProjectData } from '@/types'
 
 export const projectData: ProjectData = [
-  {
-    title: 'Work In Progress',
-    projects: [
-      {
-        text: 'IOT Security Server (with client modules)',
-        description: 'work in progress (java, C, esp-32 microcontroller)',
-        icon: 'i-carbon-campsite',
-        href: 'https://github.com/francois-coleongco/IOT-security-server',
-      },
-    ],
-  },
-  {
-    title: 'Complete',
-    projects: [
-      {
-        text: 'Multithreaded Encrypted File Server and Client (C++)',
-        description: 'multithreaded server capable of handling multiple encrypted client connections simultaneously',
-        icon: 'i-carbon-campsite',
-        href: 'https://github.com/francois-coleongco/MEFSC',
-      },
-      {
-        text: 'Encrypted Steganography Suite',
-        description: 'A GUI/CLI for embedding encrypted messages into images using LSB alpha channel encoding written in Rust',
-        icon: 'i-carbon-campsite',
-        href: 'https://github.com/Francois-Coleongco/Steganography_Suite',
-      },
-      {
-        text: 'Secure TCP Network Communications Interface in C++',
-        description: 'An easy to compile interface for securing network facing C++ projects built on top of the libsodium library',
-        icon: 'i-carbon-campsite',
-        href: '/',
-      },
-    ],
-  },
+	{
+		title: 'Projects',
+		projects: [
+			{
+				text: 'Server Agent Threat Detection (Golang + TypeScript)',
+				description: 'A microservice threat detection system to analyze telemetry from resource efficient endpoint agents',
+				icon: 'i-carbon-campsite',
+				href: 'https://github.com/francois-coleongco/SATD',
+			},
+			{
+				text: 'Multithreaded Encrypted File Server and Client (C++)',
+				description: 'multithreaded server capable of handling multiple encrypted client connections simultaneously',
+				icon: 'i-carbon-campsite',
+				href: 'https://github.com/francois-coleongco/MEFSC',
+			},
+			{
+				text: 'Encrypted Steganography Suite (Rust + JavaScript)',
+				description: 'A GUI/CLI for embedding encrypted messages into images using LSB alpha channel encoding written in Rust',
+				icon: 'i-carbon-campsite',
+				href: 'https://github.com/Francois-Coleongco/Steganography_Suite',
+			},
+			{
+				text: 'Secure TCP Network Communications Interface in C++',
+				description: 'An easy to integrate interface for securing network facing C++ projects built on top of the libsodium library',
+				icon: 'i-carbon-campsite',
+				href: '/',
+			},
+		],
+	},
 ]
